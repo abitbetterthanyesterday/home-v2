c9205f62c0ad52db8f823cb2e1438201525b7ae4
Aloys <17796338+abitbetterthanyesterday@users.noreply.github.com>
2024-03-19 22:00:52 +0100

    Add layout

--- panda.config.ts
+++ panda.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from '@pandacss/dev'
+import {createPreset} from "@park-ui/panda-preset";
 
 
 export default defineConfig({
   preflight: true,
-  presets: ['@pandacss/dev/presets', '@park-ui/panda-preset'],
+  presets: ['@pandacss/dev/presets',  createPreset({
+    accentColor: 'iris',
+    grayColor: 'slate',
+    borderRadius: 'sm',
+  }),],
   include: ['./src/**/*.{astro,js,jsx,ts,tsx}'],
   exclude: [],
   jsxFramework: 'react',
